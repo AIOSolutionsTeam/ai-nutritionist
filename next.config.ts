18858d4bdad9ae8eb7b325b161d48b6a5e6338f9
SubZed <96355118+SubZed16@users.noreply.github.com>
2026-01-31 01:11:12 +0100

    Disable trailing slash redirect to fix App Proxy loop

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,7 @@ const nextConfig: NextConfig = {
     // your project has TypeScript errors.
     ignoreBuildErrors: true,
   },
+  skipTrailingSlashRedirect: true,
   images: {
     remotePatterns: [
       {
